66221f872d0cec09254f5724aee426e01443566f
unknown <fserranozamora@gmail.com>
2024-05-21 18:00:45 -0500

    first commit

--- src/componentes/footer.jsx
+++ src/componentes/footer.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-//import redes from "../imagenes/redes.png";
-
+import redes from "../imagenes/redes.png"
 
 const CompFooter = () => {
     return (
@@ -8,6 +7,7 @@ const CompFooter = () => {
             <div className="card-body">
                 <h4 className="card-title">Diagonal 45 sur N° 13n - 04</h4>
                 <p className="card-text">correo: fserranozamora@gmail.com - Teléfono 3213473353</p>
+                <img src={redes} className="App-logo1" alt="logo"></img>
             </div>
             <div className="card-footer text-muted">Horario lunes a viernes de 8 am - 7 pm</div>
         </div>
