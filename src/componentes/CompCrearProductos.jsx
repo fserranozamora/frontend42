27adc46dc8952b485f971d7cfca0413918e730c9
fserranozamora <85131139+fserranozamora@users.noreply.github.com>
2024-05-27 19:32:13 -0500

    Update CompCrearProductos.jsx
    Añadir button

--- src/componentes/CompCrearProductos.jsx
+++ src/componentes/CompCrearProductos.jsx
@@ -6,48 +6,50 @@ import { useState } from "react";
 const URL = 'https://backend42-28ph.onrender.com/api/productos/';
 
 const CompCrearProductos = () => {
-    const [nombre_producto, setNomProd]= useState('');
-    const [unidades, setUnidades]= useState('');
-    const [precio_unitario, setPrecioUni]= useState('');
-    const [precio_total, setPrecioTotal]= useState('');
-    const navigate = useNavigate ();
+    const [nombre_producto, setNomProd] = useState('');
+    const [unidades, setUnidades] = useState('');
+    const [precio_unitario, setPrecioUni] = useState('');
+    const [precio_total, setPrecioTotal] = useState('');
+    const navigate = useNavigate();
 
     const guardarProductos = async (e) => {
         await axios.post(URL, {
-            nombre_producto:nombre_producto, 
-            unidades:unidades,
-            precio_unitario:precio_unitario,
-            precio_total:precio_total
+            nombre_producto: nombre_producto,
+            unidades: unidades,
+            precio_unitario: precio_unitario,
+            precio_total: precio_total
 
         })
         navigate('/productos')
     }
 
-    return(
+    return (
         <div>
             <h3>Formulario para guardar productos</h3>
             <form onSubmit={guardarProductos}>
                 <div className="mb-3">
-                <label className="form-label">Nombres Productos</label>
-                <input value={nombre_producto} onChange={(e) => setNomProd(e.target.value)} type="text" className="form-control"/>
+                    <label className="form-label">Nombres Productos</label>
+                    <input value={nombre_producto} onChange={(e) => setNomProd(e.target.value)} type="text" className="form-control" />
                 </div>
                 <div className="mb-3">
-                <label className="form-label">Unidades</label>
-                <input value={unidades} onChange={(e) => setUnidades(e.target.value)} type="number" className="form-control"/>
+                    <label className="form-label">Unidades</label>
+                    <input value={unidades} onChange={(e) => setUnidades(e.target.value)} type="number" className="form-control" />
                 </div>
                 <div className="mb-3">
-                <label className="form-label">Precio unitario</label>
-                <input value={precio_unitario} onChange={(e) => setPrecioUni(e.target.value)} type="text" className="form-control"/>
+                    <label className="form-label">Precio unitario</label>
+                    <input value={precio_unitario} onChange={(e) => setPrecioUni(e.target.value)} type="text" className="form-control" />
                 </div>
                 <div className="mb-3">
-                <label className="form-label">Precio total</label>
-                <input value={precio_total} onChange={(e) => setPrecioTotal(e.target.value)} type="text" className="form-control"/>
+                    <label className="form-label">Precio total</label>
+                    <input value={precio_total} onChange={(e) => setPrecioTotal(e.target.value)} type="text" className="form-control" />
                 </div>
-                <button type="submit" className="btn btn-success">  Guardar  <i className="fa-solid fa-floppy-disk"></i></button>
+                <button type="submit" className="btn btn-success"> Guardar <i className="fa-solid fa-floppy-disk"></i></button>
+                <b>&nbsp;</b>&nbsp;
+                <a href="/productos"><button type="button" className="btn btn-danger"> Cancelar <i className="fa-solid fa-xmark"></i></button></a>
             </form>
         </div>
 
-        
+
     )
 }
 
