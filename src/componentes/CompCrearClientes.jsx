d53dd9ac2e2b69dab31f9e7d26b3ddbd9511f3d7
fserranozamora <85131139+fserranozamora@users.noreply.github.com>
2024-05-27 19:28:34 -0500

    Update CompCrearClientes.jsx
    Añadir button

--- src/componentes/CompCrearClientes.jsx
+++ src/componentes/CompCrearClientes.jsx
@@ -6,59 +6,61 @@ import { useState } from "react";
 const URL = 'https://backend42-28ph.onrender.com/api/clientes/';
 
 const CompCrearClientes = () => {
-    const [nombres, setNombres]= useState('');
-    const [apellidos, setApellidos]= useState('');
-    const [cedula, setCedula]= useState('');
-    const [correo, setCorreo]= useState('');
-    const [telefono, setTelefono]= useState('');
-    const [direccion, setDireccion]= useState('');
-    const navigate = useNavigate ();
+    const [nombres, setNombres] = useState('');
+    const [apellidos, setApellidos] = useState('');
+    const [cedula, setCedula] = useState('');
+    const [correo, setCorreo] = useState('');
+    const [telefono, setTelefono] = useState('');
+    const [direccion, setDireccion] = useState('');
+    const navigate = useNavigate();
 
     const guardarClientes = async (e) => {
         await axios.post(URL, {
-            nombres:nombres, 
-            apellidos:apellidos, 
-            cedula:cedula, 
-            correo:correo, 
-            telefono:telefono, 
-            direccion:direccion
+            nombres: nombres,
+            apellidos: apellidos,
+            cedula: cedula,
+            correo: correo,
+            telefono: telefono,
+            direccion: direccion
         })
         navigate('/clientes')
     }
 
-    return(
+    return (
         <div>
             <h3>Formulario para guardar clientes</h3>
             <form onSubmit={guardarClientes}>
                 <div className="mb-3">
-                <label className="form-label">Nombres cliente</label>
-                <input value={nombres} onChange={(e) => setNombres(e.target.value)} type="text" className="form-control"/>
+                    <label className="form-label">Nombres cliente</label>
+                    <input value={nombres} onChange={(e) => setNombres(e.target.value)} type="text" className="form-control" />
                 </div>
                 <div className="mb-3">
-                <label className="form-label">Apellidos cliente</label>
-                <input value={apellidos} onChange={(e) => setApellidos(e.target.value)} type="text" className="form-control"/>
+                    <label className="form-label">Apellidos cliente</label>
+                    <input value={apellidos} onChange={(e) => setApellidos(e.target.value)} type="text" className="form-control" />
                 </div>
                 <div className="mb-3">
-                <label className="form-label">Cédula</label>
-                <input value={cedula} onChange={(e) => setCedula(e.target.value)} type="number" className="form-control"/>
+                    <label className="form-label">Cédula</label>
+                    <input value={cedula} onChange={(e) => setCedula(e.target.value)} type="number" className="form-control" />
                 </div>
                 <div className="mb-3">
-                <label className="form-label">Correo</label>
-                <input value={correo} onChange={(e) => setCorreo(e.target.value)} type="text" className="form-control"/>
+                    <label className="form-label">Correo</label>
+                    <input value={correo} onChange={(e) => setCorreo(e.target.value)} type="text" className="form-control" />
                 </div>
                 <div className="mb-3">
-                <label className="form-label">Teléfono</label>
-                <input value={telefono} onChange={(e) => setTelefono(e.target.value)} type="number" className="form-control"/>
+                    <label className="form-label">Teléfono</label>
+                    <input value={telefono} onChange={(e) => setTelefono(e.target.value)} type="number" className="form-control" />
                 </div>
                 <div className="mb-3">
-                <label className="form-label">Direccíon</label>
-                <input value={direccion} onChange={(e) => setDireccion(e.target.value)} type="text" className="form-control"/>
+                    <label className="form-label">Direccíon</label>
+                    <input value={direccion} onChange={(e) => setDireccion(e.target.value)} type="text" className="form-control" />
                 </div>
                 <button type="submit" className="btn btn-success">  Guardar  <i className="fa-solid fa-floppy-disk"></i></button>
+                <b>&nbsp;</b>&nbsp;
+                <a href="/clientes"><button type="button" className="btn btn-danger"> Cancelar <i className="fa-solid fa-xmark"></i></button></a>
             </form>
         </div>
 
-        
+
     )
 }
 
