1279caa6f1dd8d9583c8410600897cc141159f81
fserranozamora <85131139+fserranozamora@users.noreply.github.com>
2024-05-27 19:41:22 -0500

    Update CompMostrarProductos.jsx
    correccion

--- src/componentes/CompMostrarProductos.jsx
+++ src/componentes/CompMostrarProductos.jsx
@@ -7,7 +7,7 @@ const URL = 'https://backend42-28ph.onrender.com/api/productos/';
 
 const CompMostrarProductos = () => {
 
-    const[productos, setProductos] = useState([])
+    const [productos, setProductos] = useState([])
 
     useEffect(() => {
         getproductos()
@@ -22,33 +22,34 @@ const CompMostrarProductos = () => {
         await axios.delete(`${URL}${id}`)
         getproductos();
     }
-    
+
     return (
         <div className='container'>
             <h3>Lista de productos</h3>
             <div className='row'>
                 <div className='col'>
-                    <Link to= '/productos/agregar' className='btn btn-success mt-2 mb-2' >  Agregar  <i className="fa-solid fa-user-plus"></i></Link>
+                    <Link to='/productos/agregar' className='btn btn-success mt-2 mb-2' >  Agregar  <i className="fa-solid fa-user-plus"></i></Link>
                     <table className='table'>
                         <thead className="table-success">
                             <tr>
-                            <th>Nombre del producto</th>
-                            <th>Unidades</th>
-                            <th>Precio unitario</th>
-                            <th>Precio total</th>
-                            <th>Acciones</th>
+                                <th>Nombre del producto</th>
+                                <th>Unidades</th>
+                                <th>Precio unitario</th>
+                                <th>Precio total</th>
+                                <th>Acciones</th>
                             </tr>
                         </thead>
                         <tbody>
                             {productos.map((Producto, index) => (
-                                <tr key = {index}>
+                                <tr key={index}>
                                     <td>{Producto.nombre_producto}</td>
                                     <td>{Producto.unidades}</td>
                                     <td>{Producto.precio_unitario}</td>
                                     <td>{Producto.precio_total}</td>
                                     <td>
                                         <Link to={`/productos/editar/${Producto._id}`} className='btn btn-primary mt-2 mb-2'>  Editar  <i className="fa-solid fa-pen-to-square"></i></Link>
-                                        <button onClick={ () => eliminarProductos(Producto._id)} className='btn btn-danger mt-2 mb-2'>  Eliminar  <i className="fa-solid fa-trash"></i></button>
+                                        <b>&nbsp;</b>&nbsp;
+                                        <button onClick={() => eliminarProductos(Producto._id)} className='btn btn-danger mt-2 mb-2'>  Eliminar  <i className="fa-solid fa-trash"></i></button>
                                     </td>
                                 </tr>
                             ))}
